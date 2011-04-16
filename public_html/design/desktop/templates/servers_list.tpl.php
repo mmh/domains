c7d6b7104dcdac273fc0b1c5a8b237a8cc4c047d
Henrik Farre <henrik@bellcom.dk>
2011-04-16 15:29:46 +0200

    show dom0 in same table with domU

--- public_html/design/desktop/templates/servers_list.tpl.php
+++ public_html/design/desktop/templates/servers_list.tpl.php
@@ -7,26 +7,7 @@ if ( count($servers_grouped) > 0 )
   {
     $hardware = unserialize($xen0['xen0']->hardware);
 
-echo '<strong>'. $xen0['xen0']->name .'</strong> | <img src="/design/desktop/images/hardware.png" alt="hardware" class="tooltip_trigger icon"/>
-<div class="hardware tooltip">
-<table>
-  <tr>
-    <td>
-      <img src="/design/desktop/images/processor.png" alt="cpu" class="icon"/>
-    </td>
-    <td>'.$hardware['cpucount'] .' &times; '. $hardware['cpu'].'</td>
-  </tr>
-  <tr>
-    <td>
-      <img src="/design/desktop/images/ddr_memory.png" alt="memory" class="icon"/>
-    </td>
-    <td>'.$hardware['memory'].'</td>
-  </tr>
-</table>
-</div>
-<br/>
-
-<table class="tablesorter xenu">
+echo '<table class="tablesorter">
   <thead>
     <tr>
       <th class="name">Name</th>
@@ -41,7 +22,19 @@ echo '<strong>'. $xen0['xen0']->name .'</strong> | <img src="/design/desktop/ima
       <th class="comment">Comment</th>
     </tr>
   </thead>
-  <tbody>';
+  <tbody>
+    <tr class="xen0">
+      <td>'.$xen0['xen0']->name.'</td>
+      <td>'.$xen0['xen0']->ip.'</td>
+      <td class="os '.strtolower( $xen0['xen0']->os ).'">'.$xen0['xen0']->os.'</td>
+      <td>'.$xen0['xen0']->os_release.'</td>
+      <td>'.$xen0['xen0']->kernel_release.'</td>
+      <td>'.$xen0['xen0']->arch.'</td>
+      <td class="hardware cpu'. ( ( $hardware['cpucount'] ) ? '' : ' error' ) .'">'. ( ( $hardware['cpucount'] )?:'<span class="error">?</span>' ) .'</td>
+      <td class="hardware memory'. ( (empty($hardware['memory'])) ? ' error' : '' ) .'">'. ( (empty($hardware['memory'])) ? '?' : $hardware['memory'] ) .'</td>
+      <td><a class="loadDomains" href="/service/ajax/getDomains/json/?serverID='.$xen0['xen0']->id.'"><img src="/design/desktop/images/domain_template.png" /></a></td>
+      <td>'.$xen0['xen0']->comment.'</td>
+    </tr>';
 
     foreach ($xen0['xenu'] as $xenu) 
     {
