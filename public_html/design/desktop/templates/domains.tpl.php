571e0ffa3a61e39fda7ed99fc507ebf44a58cc32
Henrik Farre <henrik@bellcom.dk>
2011-04-20 21:19:05 +0200

    Domains template

--- public_html/design/desktop/templates/domains.tpl.php
+++ public_html/design/desktop/templates/domains.tpl.php
@@ -1,16 +1,32 @@
-<div id="domains" class="page">
+<div id="domains" class="page list">
+
+  <table class="tablesorter">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Owner</th>
+      </tr>
+    </thead>
+    <tbody>
+
 <?php
 
 foreach ( $accountToDomains as $account ) 
 {
-  echo '<h1>'.$account['owner']->name.'</h1>';
-  echo '<ul>';
+  echo '<tr>';
   foreach ( $account['domains'] as $domain ) 
   {
-    echo '<li><a href="http://'.$domain->name.'">'.$domain->name."</a></li>";
+    if ( $domain->type != 'name' )
+    {
+      continue;
+    }
+    echo '<td><a href="http://'.$domain->name.'">'.$domain->name."</a></td>";
   }
-  echo '</ul>';
+  echo '<td>'.$account['owner']->name.'</td>
+    </tr>';
 }
 
 ?>
+    </tbody>
+  </table>
 </div>
